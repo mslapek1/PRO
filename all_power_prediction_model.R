757f2df08ed857d61875bc3d07a6ebed0fd64012
Miki-Mal <mioodrlus0133@gmail.com>
2020-11-28 16:19:27 +0100

    wstępny model

--- all_power_prediction_model.R
+++ all_power_prediction_model.R
@@ -25,8 +25,6 @@ df <- df %>%
 
 df$day_of_week <- as.numeric( format( df$day, "%w"))
 
-df <- df_save
-
 #### na replace
 #na will be raplaced with mean of 4 neigbors in the same week_day (-14,-7,+7,+14)
 dfGi <- df$Global_intensity
@@ -70,13 +68,48 @@ all(!is.na(dfGi))
 
 df$Global_intensity <- dfGi
 
+df_save <- df
+df <- df_save
+
 #viual check if replace points are good with model
 plot(df$day, df$Global_intensity)
 df_na <- df[na_vec,]
 points(df_na$day, df_na$Global_intensity, col="red")
 
-####modeling 
+####modeling
+#assumptions:
+#1.periodic poweer usage with pick ~01.01
+#2.day of week inrelewant
 
+#day counting from begining of the year
+#[1,365] -> [0,2pi] day 366 i thaken into consideration
+funtion_day_to_cos_rad_of_day_of_year <- function(v_day){
+  
+  day_of_year <- as.numeric( strftime(v_day, format = "%j"))
+  
+  day_of_year_in_rad <- (day_of_year-1) / 365 * 2*pi
+  return( cos(day_of_year_in_rad))
+  
+}
+
+df$cos_day_of_year_in_rad <- funtion_day_to_cos_rad_of_day_of_year(df$day)
+
+plot(Global_intensity~cos_day_of_year_in_rad, df)
+
+#linear model can be aplied
+model_l <- lm(Global_intensity~cos_day_of_year_in_rad, df)
+
+summary(model_l)
+
+plot(Global_intensity~cos_day_of_year_in_rad, df)
+abline(model_l,col = 'red')
+
+
+#plot in normal
+pred <- predict(model_l, data.frame( cos_day_of_year_in_rad = df$cos_day_of_year_in_rad))
+
+plot(df$day, df$Global_intensity)
+points(df$day, pred, col="red", cex=0.1)
 
 
 
